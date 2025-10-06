598a004f8de6e08914b0ce6eea9013b9b6eec82f
Mezzano <166826256+Cleitindograu420@users.noreply.github.com>
2025-10-06 19:35:28 -0300

    DB config
    some minor configurations to the DB,
    add the user table to the DB

--- sistem/login/models.py
+++ sistem/login/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True, unique=True)
+    nome = models.TextField(max_length=150, null=False)
+    senha = models.CharField(max_length=200, null=False)
+    email = models.CharField(max_length=254, unique=True)
