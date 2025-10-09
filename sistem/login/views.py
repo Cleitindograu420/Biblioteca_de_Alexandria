1e3ff6d6d0b328ca4402f4be7bd2f42b6f72a82c
Mezzano <166826256+Cleitindograu420@users.noreply.github.com>
2025-10-08 21:53:42 -0300

    Update views.py
    criação de algumas funções dentro do views

--- sistem/login/views.py
+++ sistem/login/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from .models import Usuario,Evento, Inscrito, Certificado
 
-# Create your views here.
+#funcao para renderizar a pagina home
+#def home(request):
+# return render(request, 'usuarios/home.html')
+
+#Funcoes para usuarios--------------------------------------------------------------------------------------------------
+
+#funcao para deletar usuario de acordo com o id
+def delete_user(request, pk):
+    usuario = get_object_or_404(Usuario, pk=pk)
+    if request.method == "GET":
+        usuario.delete()
+        return redirect('usuarios/')
+
+
+
+
+
+
+#Funcoes para eventos--------------------------------------------------------------------------------------------------
+#Funcoes para inscricoes-----------------------------------------------------------------------------------------------
+#Funcoes para certificados----------------------------------------------------------------------------------------------
